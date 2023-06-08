2511154238ae3f5bcd55a9a0990e5f5a29d64fc4
Ananda Dwi Mirza <ananda120206@gmail.com>
2023-06-08 21:27:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+const tabButton = document.querySelectorAll(".tab-btn");
+const tabContent = document.querySelectorAll(".card-content");
+const tabCard = document.querySelectorAll(".card");
+
+tabButton.forEach((tab, index) => {
+    tab.addEventListener("click", (e) => {
+        tabButton.forEach(tab => {tab.classList.remove("active")});
+        tab.classList.add("active");
+
+    tabContent.forEach(tab => {tab.classList.remove("active")});
+    tabContent[index].classList.add("active");
+    });
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    spaceBetween: 20,
+    centeredSlides: true,
+
+    loopFillGroupWithBlank: true,
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
